87720aa134a20fb74bf3989a16909495911d80bf
Andres <andresrosalespinzon@gmail.com>
2021-04-09 17:00:09 +0200

    app done but bug with favorites page when there are in the list

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,27 +1,6 @@
 import { useState, useEffect } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
-// const DUMMY_DATA = [
-//   {
-//     id: 'm1',
-//     title: 'This is a first meetup @Berlin',
-//     image:
-//       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-//     address: 'Meetupstreet 5, 12345 Berlin',
-//     description:
-//       'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-//   },
-//   {
-//     id: 'm2',
-//     title: 'This is a second meetup @Barcelona',
-//     image:
-//       'https://tecnohotelnews.com/wp-content/uploads/2019/05/shutterstock_214016374.jpg',
-//     address: 'Meetupstreet 5, 12345 Barcelona',
-//     description:
-//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, omnis rem in quo repellat incidunt et pariatur laudantium, fugiat, porro veritatis maxime ipsam assumenda ab at aperiam? Laboriosam, quisquam dicta. Nostrum velit sunt sapiente quis!',
-//   },
-// ];
-
 function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,22 @@
+import { useContext } from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+
+import FavoritesContext from '../store/favorites-context';
+
 function FavoritesPage() {
-  return <div>All favorites Page</div>;
+  const favoritesCtx = useContext(FavoritesContext);
+  let content;
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You got no favorites trips yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList MeetupList={favoritesCtx.favorites} />;
+  }
+  return (
+    <section>
+      <h2>My Favorites</h2>
+      {content}
+    </section>
+  );
 }
 
 export default FavoritesPage;
