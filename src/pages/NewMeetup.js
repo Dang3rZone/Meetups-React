f97352c9d59f197ea147f4ab9991523b8c1d76cc
Andres <andresrosalespinzon@gmail.com>
2021-04-09 11:37:13 +0200

    linked with firebase to store meetups

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,22 @@
+import { useHistory } from 'react-router-dom';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-  function addMeetupHandler(meetupHandler) {}
+  const history = useHistory();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-worldmeetups-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then(() => {
+      history.replace('/');
+    });
+  }
   return (
     <section>
       <h1>Add new Meetup</h1>
