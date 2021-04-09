fab07a738f079c388701d53d040e1ad5f3f7fd64
Andres <andresrosalespinzon@gmail.com>
2021-04-09 11:13:21 +0200

    handler for adding a new meetup

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,11 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupHandler) {}
   return (
     <section>
       <h1>Add new Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
